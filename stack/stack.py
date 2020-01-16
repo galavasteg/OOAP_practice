67e69004053214f795c2d9c46c173dd4373ca61f
Artem Shakurov <ashakurov@gmcs.ru>
2020-01-16 11:50:45 +0300

    #1 Stack: constants, tests

---
+++ stack/stack.py
@@ -0,0 +1,14 @@
+from .abstract_data_types.abstract_stack import AbstractStack
+
+
+class Stack(AbstractStack):
+
+    # class interface that implements AbstractStack
+    POP_NIL = 0  # pop() not called yet
+    POP_OK = 1   # last pop() call completed successfully
+    POP_ERR = 2  # stack storage is empty
+
+    PEEK_NIL = 0  # peek() not called yet
+    PEEK_OK = 1   # last peek() call returned correct value
+    PEEK_ERR = 2  # stack storage is empty
+
