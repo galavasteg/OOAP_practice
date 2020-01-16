b0e66f0f0d4e0d4e0d9961edbeff17e341083f87
Artem Shakurov <ashakurov@gmcs.ru>
2020-01-16 11:42:16 +0300

    #1 Abstract Stack: constructor

--- stack/abstract_data_types/abstract_stack.py
+++ stack/abstract_data_types/abstract_stack.py
@@ -14,6 +14,18 @@ CONSTANTS
 
     PEEK_ERR = 2  # stack storage is empty
 
+CONSTRUCTOR
+    __new__(cls) -> new class instance
+        Post-condition:
+            created a new empty stack instance.
+
+        NOTE: __new__ is NOT abstract method. You shouldn't
+            need to override __new__. See:
+            https://mail.python.org/pipermail/tutor/2008-April/061426.html
+
+    __init__(self):
+        Initializing the instance after it's been created.
+
 """
 
 from abc import ABCMeta, abstractmethod
@@ -27,5 +39,23 @@ class AbstractStack(metaclass=ABCMeta):
     PEEK_NIL = 0  # peek() not called yet
     PEEK_OK = 1   # last peek() call returned correct item
     PEEK_ERR = 2  # stack storage is empty
+
+    # constructor
+    def __new__(cls) -> object:
+        """
+        Create a class instance
+        Post-condition:
+            created a new empty stack instance
+
+        NOTE: __new__ is NOT abstract method. You shouldn't
+            need to override __new__. See:
+            https://mail.python.org/pipermail/tutor/2008-April/061426.html
+        """
+        new_instance = super().__new__(cls)
+        return new_instance
+
+    @abstractmethod
+    def __init__(self):
+        """Initializing the instance after it's been created"""
 
 
