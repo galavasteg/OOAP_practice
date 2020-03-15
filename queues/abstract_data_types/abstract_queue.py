0da8fb829824db382f8833af5b3d08aa69335d65
Artem Shakurov <galavasteg@gmail.com>
2020-03-15 16:44:15 +0300

    # 5 AbstractQueue: constants, constructor, statuses

--- queues/abstract_data_types/abstract_queue.py
+++ queues/abstract_data_types/abstract_queue.py
@@ -55,16 +55,43 @@ from abc import ABCMeta, abstractmethod
 
 class AbstractQueue(metaclass=ABCMeta):
 
+    PEEK_NIL = 0        # peek() not called yet
+    PEEK_OK = 1         # last peek() call returned correct item
+    PEEK_EMPTY_ERR = 2  # queue is empty
+
+    DEQUEUE_NIL = 0        # dequeue() not called yet
+    DEQUEUE_OK = 1         # last dequeue() call completed successfully
+    DEQUEUE_EMPTY_ERR = 2  # queue is empty
+
     # constructor
     def __new__(cls) -> object:
         """
-        Create a class instance
-        Post-condition: created a new instance with empty storage
+        Create a class instance.
+        Post-condition: created a new instance.
         """
         new_instance = super().__new__(cls)
         return new_instance
 
     @abstractmethod
     def __init__(self):
-        """Initializing the instance after it's been created"""
+        """Initializing the instance after it's been created
+
+        Post-condition:
+            - the queue command statuses set to initial (*_NIL constants).
+            - the queue size is 0.
+
+        """
+
+    # method statuses requests:
+    @abstractmethod
+    def get_peek_status(self) -> int:
+        """Return status of last peek() call:
+        one of the PEEK_* constants."""
+        return 0
+
+    @abstractmethod
+    def get_dequeue_status(self) -> int:
+        """Return status of last dequeue() call:
+        one of the DEQUEUE_* constants."""
+        return 0
 
