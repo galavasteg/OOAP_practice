ebcecded93ba57c0054b8a1790af2971e5697477
Artem Shakurov <galavasteg@gmail.com>
2020-03-27 19:31:51 +0300

    #6 Ref queues: deque extends queue interface

--- queues/queues.py
+++ queues/queues.py
@@ -26,6 +26,15 @@ class __BaseQueue(_BaseAbstractQueue):
 
             self._remove_status = self.REMOVE_OK
 
+    # commands:
+    def enqueue(self, item):
+        new_tail_index = len(self)
+        self._queue.insert(new_tail_index, item)
+
+    def dequeue(self):
+        head_i = 0
+        self._remove_item_by_index(head_i)
+
     # additional requests:
     def _get_item_by_index(self, item_index: int) -> object:
         if len(self) == 0:
@@ -57,15 +66,7 @@ class __BaseQueue(_BaseAbstractQueue):
 
 
 class Queue(__BaseQueue, AbstractQueue):
-
-    # commands:
-    def enqueue(self, item):
-        new_tail_index = len(self)
-        self._queue.insert(new_tail_index, item)
-
-    def dequeue(self):
-        head_i = 0
-        self._remove_item_by_index(head_i)
+    ...
 
 
 class Deque(__BaseQueue, AbstractDeque):
@@ -76,12 +77,10 @@ class Deque(__BaseQueue, AbstractDeque):
         self._queue.insert(head_i, item)
 
     def add_tail(self, item):
-        new_tail_index = len(self)
-        self._queue.insert(new_tail_index, item)
+        self.enqueue(item)
 
     def remove_front(self):
-        head_i = 0
-        self._remove_item_by_index(head_i)
+        self.dequeue()
 
     def remove_tail(self):
         tail_i = len(self) - 1
