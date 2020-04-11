ac0d549ea19bedc6da1da99f4ba7608133a01546
Artem Shakurov <galavasteg@gmail.com>
2020-04-11 23:32:18 +0300

    #7 HashTable: collisions tests

--- hash_table/test_hash_table.py
+++ hash_table/test_hash_table.py
@@ -71,35 +71,42 @@ class HT2PartialFilledTests(HTTestsBase):
         self.assertEqual(self.ht.get_put_status(),
                          HashTable.PUT_OK)
 
-    def test_02_partial_filled_put_bad_exists(self):
+    def test_02_partial_filled_put_ok_collision(self):
+        self.ht.put('r')
+
+        self.assertTrue(self.ht.is_value('r'))
+        self.assertEqual(self.ht.get_put_status(),
+                         HashTable.PUT_OK)
+
+    def test_03_partial_filled_put_bad_exists(self):
         self.ht.put('ab')
 
         self.assertTrue(self.ht.is_value('ab'))
         self.assertEqual(self.ht.get_put_status(),
                          HashTable.PUT_EXISTS_ERR)
 
-    def test_03_partial_filled_put_bad_collision(self):
+    def test_04_partial_filled_put_bad_collision(self):
         self.ht.put('ba')
 
         self.assertFalse(self.ht.is_value('ba'))
         self.assertEqual(self.ht.get_put_status(),
                          HashTable.PUT_COLLISION_ERR)
 
-    def test_04_partial_filled_remove_ok(self):
+    def test_05_partial_filled_remove_ok(self):
         self.ht.remove('ab')
 
         self.assertFalse(self.ht.is_value('ab'))
         self.assertEqual(self.ht.get_remove_status(),
                          HashTable.REMOVE_OK)
 
-    def test_05_partial_filled_remove_bad(self):
+    def test_06_partial_filled_remove_bad(self):
         self.ht.remove('val')
 
         self.assertFalse(self.ht.is_value('val'))
         self.assertEqual(self.ht.get_remove_status(),
                          HashTable.REMOVE_NOVALUE_ERR)
 
-    def test_06_partial_filled_remove_bad_rm_collision(self):
+    def test_07_partial_filled_remove_bad_rm_collision(self):
         self.ht.remove('ba')
 
         self.assertTrue(self.ht.is_value('ab'))
@@ -142,9 +149,37 @@ class HT3FilledTests(HTTestsBase):
         self.assertEqual(self.ht.get_remove_status(),
                          HashTable.REMOVE_NOVALUE_ERR)
 
+
+class HT4CollisionTests(HTTestsBase):
+
+    HT_SIZE = 10
+    INIT_ITEMS = ('2', '3', '4',
+                  'n', '6', '7',
+                  'x', '9', '0',
+                  '5',)
+                 # ^collisions
+
+    def setUp(self):
+        HashTable.STEP = 3
+        super().setUp()
+
+    def test_01_remove_collision_with_rebalance(self):
+        print(tuple(self.ht._values))
+        self.ht.remove('2')
+
+        actual_values = tuple(self.ht._values)
+        expected_values = ('x', '3', '4',
+                           'n', '6', '7',
+                           '5', '9', '0',
+                           None,)
+                          # ^ collisions offset
+
+        self.assertTupleEqual(actual_values, expected_values)
+
+
 class HT5RemoveAllTests(HTTestsBase):
 
-    HT_SIZE = 10000
+    HT_SIZE = 1000
     INIT_ITEMS = tuple(map(str, range(HT_SIZE)))
 
     def test_01_remove_all(self):
