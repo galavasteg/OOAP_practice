80d7e1e0b5fdec9fda5c67ec82c4649be8f36172
Artem Shakurov <galavasteg@gmail.com>
2020-05-10 03:21:16 +0300

    #10 BloomFilter: hash functions

--- bloom_filter.py
+++ bloom_filter.py
@@ -29,6 +29,9 @@ REQUESTS
 
 """
 
+from functools import reduce
+
+
 class BloomFilter:
 
     def __init__(self, size: int):
@@ -41,6 +44,70 @@ class BloomFilter:
         :param size: filter size.
 
         """
+        assert size > 0, 'The size must greater than 0.'
+        self._busy_bit_set = set()
+        self._size = size
+
+    # additional requests:
+    def _hash1(self, value: str) -> int:
+        """
+        Map the **value** to the bit position on the
+        filter bit mask.
+
+        :param value: string.
+        :return: bit position as positive integer.
+
+        >>> BloomFilter(5)._hash1('foo')
+        1
+
+        """
+        multiplier = 17
+
+        def renew_hash(prev_hash: int, char: str) -> int:
+            accum_hash = prev_hash * multiplier + ord(char)
+            hash_ = accum_hash % self._size
+            return hash_
+        hash_ = reduce(renew_hash, value, 0)
+
+        return hash_
+
+    def _hash2(self, value: str) -> int:
+        """
+        Map the **value** to the bit position on the
+        filter bit mask.
+
+        :param value: string.
+        :return: bit position as positive integer.
+
+        >>> BloomFilter(5)._hash2('foo')
+        4
+
+        """
+        b_str = value.encode()
+        hash_ = sum(b_str) % self._size
+        return hash_
+
+    def _get_value_bit_positions(self, value: str) -> tuple:
+        """
+        Return mapped **value** on the filter bit mask
+        with the hash* functions.
+
+        :param value: string value.
+        :return: a set of bits.
+
+        >>> bf = BloomFilter(5)
+        >>> bf.add('foo')
+        >>> bf._get_value_bit_positions('foo')
+        (1, 4)
+        >>> bf._get_bit_mask()
+        '10010'
+
+        """
+        hashes = (
+            self._hash1(value),
+            self._hash2(value),
+        )
+        return hashes
 
     # commands:
     def add(self, value: str) -> None:
@@ -67,3 +134,8 @@ class BloomFilter:
         :param value: string.
 
         """
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
