d8625a3a97d3c24d20f07789d59f23574ca62a66
Artem Shakurov <galavasteg@gmail.com>
2020-05-24 18:30:34 +0300

    #11 Class as module in Python: task 3

--- part_2_OOP/iInheritance_composition_polymorphism.py
+++ part_2_OOP/iInheritance_composition_polymorphism.py
@@ -8,6 +8,10 @@ Write a small code example with comments where
 inheritance applies both the extension of the parent class
 and the specialization of the parent class.
 
+Task 3.
+Explain how the "class as a module" concept is supported
+in Python.
+
 """
 
 
@@ -71,3 +75,34 @@ class MotoMonocycle(Bicycle):
 
         print('I can a-ron-don-don!')
 
+
+# -------------------- Task 3 -----------------------
+# Explain how the "class as a module" concept is supported
+# in Python.
+
+"""
+There are huge differences between classes and modules in Python.
+
+Classes are blueprints that allow you to create instances with
+attributes and bound functionality. Classes support inheritance,
+metaclasses, and descriptors.
+
+Modules can't do any of this, modules are essentially singleton
+instances of an internal module class, and all their globals are
+attributes on the module instance. You can manipulate those attributes
+as needed (add, remove and update), but take into account that these
+still form the global namespace for all code defined in that module.
+
+Modules can contain more than just one class however; functions
+and any the result of any other Python expression can be globals
+in a module too.
+
+So as a general ballpark guideline:
+    Use classes as blueprints for objects that model your problem domain.
+    Use modules to collect functionality into logical units.
+
+Global state goes in modules (and functions and classes are just as much
+global state, loaded at the start). Everything else goes into other data
+structures, including instances of classes.
+
+"""
